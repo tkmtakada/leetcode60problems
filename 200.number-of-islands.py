a20ccd0c7448223e5c2fe8aef989f0d71b888e54
tkmtakada <tkmtakada@gmail.com>
2022-07-18 22:39:53 +0900

    solve numIslands

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -7,6 +7,27 @@
 # @lc code=start
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # 方針
+        self.H, self.W = len(grid), len(grid[0])
+        counter = 0
+        for i in range(self.H):
+            for j in range(self.W):
+                if grid[i][j] == "1":  # 未知の島
+                    self.dfs(grid, i, j)
+                    counter += 1
+        return counter
+
+    def dfs(self, grid, i, j):
+        # --- 1を#に変える ---
+        # 終了条件
+        if i<0 or j <0 or i>=self.H or j>=self.W or grid[i][j] != "1":
+            return 
         
+        grid[i][j] = "#"
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j-1)
+
 # @lc code=end
 
