44a4efbc55222c03d8f3a7539335344eed0729a0
tkmtakada <tkmtakada@gmail.com>
2022-08-11 23:37:48 +0900

    review

---
+++ 560.subarray-sum-equals-k copy.py
@@ -0,0 +1,15 @@
+#
+#
+# @lc app=leetcode id=560 lang=python3
+#
+# [560] Subarray Sum Equals K
+#
+
+# @lc code=start
+from collections import deque
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+                    
+# @lc code=end
+
--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -1,12 +1,36 @@
 #
+#
 # @lc app=leetcode id=560 lang=python3
 #
 # [560] Subarray Sum Equals K
 #
 
 # @lc code=start
+from collections import deque
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        """
+        2022/08/11
+        """
+        cnt = 0
+        mp = {0:1}  # cumsum: num
+        total = 0
+        for num in nums:
+            total += num
+
+            if (total - k) in mp:
+                cnt += mp[total-k]
+            if total in mp:
+                mp[total] += 1
+            else:
+                mp[total] = 1
+            
+
+        return cnt
+    def solve(self, nums, k):
+        """
+        以前といた開放
+        """
         cumsum = [0]
         total = 0
         mp = {0:1}
@@ -24,6 +48,7 @@ class Solution:
                 mp[total] = 1            
 
         return ans
+
                     
 # @lc code=end
 
