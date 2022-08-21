a18d7ba729ae19864438b89f8e0f312c19ded524
tkmtakada <tkmtakada@gmail.com>
2022-08-21 22:41:34 +0900

    solved

--- 121.best-time-to-buy-and-sell-stock.py
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -7,6 +7,21 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        2022/08/18
+        """
+        minv, maxv = float('inf'), prices[0]
+        maxdif = 0
+        for price in prices:
+            if price < minv:
+                minv = price
+                maxv = -float('inf')
+            elif price > maxv:
+                maxv = price
+                maxdif = max(maxdif, maxv-minv)
+        return maxdif
+        
+    def maxProfit2(self, prices: List[int]) -> int:
         max_profit = 0
         min_v = float('inf')
         max_v = prices[0]
--- 122.best-time-to-buy-and-sell-stock-ii.py
+++ 122.best-time-to-buy-and-sell-stock-ii.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        2022/08/19
+        """        
+        total = 0
+        for i in range(1, len(prices)):
+            total += max(prices[i] - prices[i-1], 0)
+
+        return total
+    def maxProfit2(self, prices: List[int]) -> int:
         total = 0
         minv = prices[0]  # float('inf')
         maxv = prices[0]
