ec617264f13cf3ce543f7520fc7be17e5f3046a4
tkmtakada <tkmtakada@gmail.com>
2022-08-31 00:32:41 +0900

    solved

--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -7,6 +7,36 @@
 # @lc code=start
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        """
+        2022/08/30
+        """
+        # exception case
+        if target < nums[0]: return 0
+        if target > nums[-1]: return len(nums)
+
+        s = 0
+        e = len(nums)-1
+        while s+1 < e:
+            m = (s + e) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                s = m 
+            elif nums[m] > target:
+                e = m
+
+        # 見つからなかった時の処理が欠けていない
+        if nums[s]==target:
+            return s
+        elif nums[e]==target:
+            return e
+        else:
+            return e
+            
+
+    
+
+    def searchInsert2(self, nums: List[int], target: int) -> int:
         # 例外地処理
         if target < nums[0]: return 0
         elif nums[-1] < target: return len(nums)
