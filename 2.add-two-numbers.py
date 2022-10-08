e2a5a5c01bfcf78f69b8b417aa2092a5a187a903
tkmtakada <tkmtakada@gmail.com>
2022-10-08 22:53:06 +0900

    solved

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -12,6 +12,35 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        ans = ListNode(0)
+        cur = ans
+        k = 0
+        while True:
+            if l1 is None and l2 is None and k == 0:
+                break
+            
+            if l1 is None:
+                l1_val = 0
+            else:
+                l1_val = l1.val
+                l1 = l1.next
+            
+            if l2 is None:
+                l2_val = 0
+            else:
+                l2_val = l2.val
+                l2 = l2.next
+            
+            sum_digits = l1_val + l2_val + k
+            r = sum_digits % 10
+            cur.next = ListNode(r)            
+            k = sum_digits // 10
+
+            # update
+            cur = cur.next
+
+        return ans.next
+    def addTwoNumbers2(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         ans_str = []
         head = ListNode(0, None)
         cur = head
---
+++ 216.combination-sum-iii.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=216 lang=python3
+#
+# [216] Combination Sum III
+#
+
+# @lc code=start
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        
+# @lc code=end
+
