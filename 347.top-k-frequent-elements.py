b4127f758a399980e846376637242b6cfd0918fd
tkmtakada <tkmtakada@gmail.com>
2022-10-12 00:31:15 +0900

    solving

--- 347.top-k-frequent-elements.py
+++ 347.top-k-frequent-elements.py
@@ -14,6 +14,26 @@ class Solution:
                 mp[num] += 1
             else:
                 mp[num] = 1
+        print("k ", k)
+        print("mp: ", mp)
+        queue = []
+        heapq.heapify(queue)
+        for k, v in mp.items():
+            heapq.heappush(queue, (v, k))
+            print("length of queue", len(queue))
+            while len(queue) > k-1:
+                heapq.heappop(queue)
+            print("queue", queue)
+        return [elt[1] for elt in queue]
+
+
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
+        mp = {}
+        for num in nums:
+            if num in mp:
+                mp[num] += 1
+            else:
+                mp[num] = 1
         
         # 各数字の出現頻度はわかった
         # heapでtop kを出していく？？
